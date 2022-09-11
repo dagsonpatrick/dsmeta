a16fb60426ec477c2b6abe538710a92f96ec7137
dagsopatrick <dagsonmg@yahoo.com.br>
2022-09-11 20:58:26 -0300

    Implementacao do BackEnd

---
+++ backend/src/main/java/controller/SaleController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.dto.SaleDTO;
+
+import services.SaleService;
+import services.SmsService;
+
+/**
+ * @author Dagson Souza
+ *
+ */
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@Autowired
+	private SmsService serviceSms;
+	
+	 
+	@GetMapping
+	public Page<SaleDTO> findAll(Pageable pageable){
+		return service.findAll(pageable);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public SaleDTO findById(@PathVariable Long id){
+		return service.findById(id);
+	}
+	
+	public Page<SaleDTO> findSales(@RequestParam(value="minDate" , defaultValue="") String minDate,
+							 @RequestParam(value="maxDate" , defaultValue="") String maxDate,
+							 Pageable pageable){
+		
+		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		serviceSms.sendSms(id);
+	}
+	
+	
+
+}
---
+++ backend/src/main/java/services/SaleService.java
@@ -0,0 +1,55 @@
+package services;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.dto.SaleDTO;
+import com.devsuperior.dsmeta.entities.Sale;
+
+import repositories.SaleRepository;
+
+/**
+ * @author Dagson Souza
+ *
+ */
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+
+	public Page<SaleDTO> findAll(Pageable pegeable){
+		Page<Sale> result = repository.findAll(pegeable);
+		Page<SaleDTO> page = result.map(x -> new SaleDTO(x));
+		return page;
+	}
+	
+	public SaleDTO findById(Long id){
+		Sale result = repository.findById(id).get();
+		SaleDTO dto = new SaleDTO(result);
+		return dto;
+	}
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	public Page<SaleDTO> findSales(String minDate, String maxDate, Pageable pageable){		
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		Page<Sale> result = repository.findSales(min, max, pageable);
+		Page<SaleDTO> page = result.map(x -> new SaleDTO(x));
+		return page;
+	}
+
+}
